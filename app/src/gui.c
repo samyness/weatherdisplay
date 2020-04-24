9e3d739195c75c38c574ddfb31cadb2b6a7f9c0b
samyness <63192089+samyness@users.noreply.github.com>
2020-04-24 06:54:46 +0200

    - Language: DE

--- app/src/gui.c
+++ app/src/gui.c
@@ -184,7 +184,7 @@ void ICACHE_FLASH_ATTR drawForecast(Forecast *forecast, int count)
 			tempMax > -100 && tempMax < 100)	// some sense
 		{
 			char strbuf[20] = "";
-			os_sprintf(strbuf, "%d°...%d°", tempMin, tempMax);
+			os_sprintf(strbuf, "%dÂ°...%dÂ°", tempMin, tempMax);
 			gfxDrawStrCentred(tempFont, textCentre[i], tempY, strbuf);
 		}
 	}
@@ -290,12 +290,12 @@ const char* ICACHE_FLASH_ATTR epochToWeekdayStr(uint epoch)
 		{
 			if (weekday == curTime.tm_wday)
 			{
-				return "Today";
+				return "Heute";
 			}
 //			else if (weekday == curTime.tm_wday+1 ||
 //					(weekday == 0 && curTime.tm_wday == 6))
 //			{
-//				return "Tomorrow";
+//				return "Morgen";
 //			}
 			else
 			{
@@ -317,20 +317,20 @@ const char* ICACHE_FLASH_ATTR weekdayToString(int tm_wday)
 {
 	switch (tm_wday)
 	{
-//	case 0: return "Sunday";
-//	case 1: return "Monday";
-//	case 2: return "Tuesday";
-//	case 3: return "Wednesday";
-//	case 4: return "Thursday";
-//	case 5: return "Friday";
-//	case 6: return "Saturday";
-	case 0: return "Sun";
-	case 1: return "Mon";
-	case 2: return "Tue";
-	case 3: return "Wed";
-	case 4: return "Thu";
-	case 5: return "Fri";
-	case 6: return "Sat";
+//	case 0: return "Sonntag";
+//	case 1: return "Montag";
+//	case 2: return "Dienstag";
+//	case 3: return "Mittwoch";
+//	case 4: return "Donnerstag";
+//	case 5: return "Freitag";
+//	case 6: return "Samstag";
+	case 0: return "So";
+	case 1: return "Mo";
+	case 2: return "Di";
+	case 3: return "Mi";
+	case 4: return "Do";
+	case 5: return "Fr";
+	case 6: return "Sa";
 	default: return "";
 	}
 }
